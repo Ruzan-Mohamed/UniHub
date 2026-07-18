9587b09846db271ba736385201b07d026eafb84b
Mohamed Rishan Mohamed Ruzan <mohamedruzan.pro@gmail.com>
2026-07-18 21:54:30 +0530

    create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UniHub</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <div class="nav-brand">
+            <img src="./images/logo/logo.png" alt="UniHub Logo">
+            <h1>UniHub</h1>
+        </div>
+        
+        <ul>
+            <li><a href="./auth/login.php">Sign In </a></li>
+            <li><a href="./auth/register.php">Register</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
